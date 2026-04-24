efca1d65951a190f3eac629c7059f2aecfe61c52
Nueljs <manuelcerveralara@gmail.com>
2026-04-24 20:00:17 +0200

    parser complete

--- config_parser.py
+++ config_parser.py
@@ -16,21 +16,42 @@ class MazeConfig:
 def parse_config(path: str) -> MazeConfig:
     try:
         with open(path, 'r') as f:
-            config: MazeConfig
+            raw: dict = {}
             for line in f:
+                if line.strip() == '':
+                    continue
                 if line[0] == '#':
                     continue
-                if line == '\0':
-                    continue
                 else:
-                    data: list = line.split('=')
-                    if data[0] == "width" or data[0] == "height" or data[0] == "seed":
-                        config.data[0] = int(data[1])
-                    
+                    data: list = line.split('=', 1)
+                    if len(data) != 2:
+                        print("Error: invalid line in config:"
+                              f" '{line.strip()}'")
+                        sys.exit(1)
+                    raw[data[0]] = data[1].strip()
+            config: MazeConfig = MazeConfig(
+                width=int(raw['WIDTH']),
+                height=int(raw['HEIGHT']),
+                entry=(
+                    int(raw['ENTRY'].split(',')[0]),
+                    int(raw['ENTRY'].split(',')[1])),
+                exit_pos=(
+                    int(raw['EXIT'].split(',')[0]),
+                    int(raw['EXIT'].split(',')[1])),
+                output_file=raw['OUTPUT_FILE'],
+                perfect=raw['PERFECT'] == 'True',
+                seed=int(raw["SEED"]) if "SEED" in raw else None
+            )
     except FileNotFoundError:
         print(f"Error: config file '{path}' not found")
         sys.exit(1)
     except PermissionError:
         print(f"Error: You don't have permission on file '{path}'")
+        sys.exit(1)
+    except KeyError as e:
+        print(f"Error: missing mandatory key {e} in config file")
+        sys.exit(1)
+    except ValueError as e:
+        print(f"Error: {e}")
         sys.exit(1)
     return config
