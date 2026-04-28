19abb92d6e1696dfd21533cadc0e34a894ab9008
rpedreno <raulpedrenoperez@gmail.com>
2026-04-28 13:30:52 +0200

    instalable and makefile

--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -1,9 +1,8 @@
-"""
+from .config_parser import MazeConfig, parse_config
 from .generator import MazeGenerator
-from .config_parser import parse_config
-
 
 __all__ = [
-    "MazeGenerator", "parse_config"
+    "MazeConfig",
+    "parse_config",
+    "MazeGenerator",
 ]
-"""
